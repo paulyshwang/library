0199353284626a4f5c82512a36e57703357abadd
Paul YS Hwang <109876624+paulyshwang@users.noreply.github.com>
2024-01-29 19:51:50 -0500

    Add page count and read status
    Also adds basic validation for page count input (only accepts positive
    integers) and a button to toggle read status.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const form = document.querySelector("form");
 // Get book info from form
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
 
 let id = 0;
 const myLibrary = [];
@@ -14,12 +16,19 @@ const myLibrary = [];
 addButton.addEventListener("click", (event) => {
   event.preventDefault();
 
-  // TODO: Validate input before submitting
+  // Validate input before submitting
   if (
     inputTitle.value.trim() === "" 
     || inputAuthor.value.trim() === ""
+    || inputPages.value === ""
     ) {
     alert("All fields must be filled out.");
+    return;
+  } else if (
+    !Number.isInteger(Number(inputPages.value))
+    || inputPages.value < 1
+    ) {
+    alert("Number of pages must be a positive integer.");
     return;
   }
 
@@ -28,15 +37,17 @@ addButton.addEventListener("click", (event) => {
 });
 
 // Functions
-function Book(title, author) {
+function Book(title, author, pages, read) {
   this.id = id++;
   this.title = title;
   this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // Create book object
-  const book = new Book(inputTitle.value.trim(), inputAuthor.value.trim());
+  const book = new Book(inputTitle.value.trim(), inputAuthor.value.trim(), parseInt(inputPages.value), inputRead.checked);
 
   // Add book to myLibrary
   myLibrary.push(book);
@@ -47,19 +58,41 @@ function addBookToLibrary() {
 
 function displayBooks() {
   // Clear table before re-displaying
-  table.innerHTML = "<tr><th>Title</th><th>Author</th></tr>";
+  table.innerHTML = "<tr><th>Title</th><th>Author</th><th>Pages</th><th>Read</th></tr>";
 
   for (let book of myLibrary) {
     // Create table row elements
     const row = document.createElement("tr");
     const rowTitle = document.createElement("td");
     const rowAuthor = document.createElement("td");
+    const rowPages = document.createElement("td");
+    const rowRead = document.createElement("td");
+    const readButton = document.createElement("button");
     const removeButton = document.createElement("button");
 
     // Add text content to row elements
     rowTitle.textContent = book.title;
     rowAuthor.textContent = book.author;
+    rowPages.textContent = book.pages;
+    if (book.read) {
+      readButton.textContent = "Read";
+    } else {
+      readButton.textContent = "Unread";
+    }
     removeButton.textContent = "Remove";
+
+    // Add event listener to read button
+    readButton.addEventListener("click", () => {
+      // Toggles read status on book object
+      book.read = !book.read;
+
+      // Toggles read status on display
+      if (book.read) {
+        readButton.textContent = "Read";
+      } else {
+        readButton.textContent = "Unread";
+      }
+    });
 
     // Add event listener to remove button
     removeButton.addEventListener("click", () => {
@@ -78,6 +111,9 @@ function displayBooks() {
     // Append row elements to row, then row to table
     row.appendChild(rowTitle);
     row.appendChild(rowAuthor);
+    row.appendChild(rowPages);
+    rowRead.appendChild(readButton);
+    row.appendChild(rowRead);
     row.appendChild(removeButton);
 
     table.appendChild(row);
